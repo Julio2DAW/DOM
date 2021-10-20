a3faa1e485fd9d7c7b13a1127f00786e276a40ec
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-10-20 09:35:11 +0200

    v4

--- js/dom.js
+++ js/dom.js
@@ -32,14 +32,15 @@ function ejecutar(){
 }
 
 
-  setInterval(mover, 100)
+setInterval(mover, 100)
 
-  let posicion = 0
-  function mover() {
-    let p = document.getElementById('correCaminos')
-    p.style.left = `${posicion}px`
-    posicion += 1
-  }
+let posicion = 0
+function mover() {
+  let p = document.getElementById('correCaminos')
+  p.style.left = `${posicion}px`
+  posicion += 1
+}
+
 function construir() {
   console.log('Construyo')
   let div = document.getElementsByClassName('borde')[0]
@@ -47,4 +48,30 @@ function construir() {
   //div.innerHTML = '<p>Esto es un texto <span class="negrita">en negrita</span>.</p>'
   let p = document.createElement('p')
   div.appendChild(p)
+  let nodoTexto = document.createTextNode('Este es un texto...')
+  p.appendChild(nodoTexto)
+
+  let span = document.createElement('span')
+  p.appendChild(span)
+  span.classList.add('negrita')
+  //span.classList.add('borde')
+  span.appendChild(document.createTextNode('en negrita'))
+  let img = document.createElement('img')
+  div.insertBefore(img, p)
+  img.setAttribute('src', 'img/doom.jfif')
+  //document.body.appendChild(img)
+}
+
+function destruir() {
+  let p = document.querySelector('.borde p')
+  //p = document.getElementsByTagName('p')[2]
+  //p = document.getElementByClassName('borde')[0].getElementsByTagName('p')[0]
+  console.log(p.innerHTML)
+  p.remove()
+}
+
+function destruirTodo() {
+  let div = document.getElementsByClassName('borde')[0]
+  while (div.firstElementChild)
+    div.removeChild(div.firstElementChild)
 }
